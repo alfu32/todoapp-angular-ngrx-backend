6ac273a4ceb873a715a00caa943087de6bf0ea09
alfu32 <ioan.alferaru@gmail.com>
2019-06-01 08:03:01 +0200

    https://github.com/alfu32/todoapp-angular-ngrx-backend/issues/2 returns storage controller response

--- controllers/todos.controller.js
+++ controllers/todos.controller.js
@@ -62,7 +62,7 @@ exports.removeTodo = async function (req, res, next) {
     var id = req.params.id;
     try {
         var deleted = await TodoService.deleteTodo(id)
-        return res.status(204).json({ status: 204, data: deleted, message: "Succesfully Todo Deleted" })
+        return res.status(200).json({ status: 200, data: deleted, query:req.params, message: "Succesfully Todo Deleted" })
     } catch (e) {
         return res.status(400).json({ status: 400, message: e.message })
     }
